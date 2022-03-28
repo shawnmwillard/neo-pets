a8d26323613c45945398f5695b37c66369890aa0
EsteMaeLeo <leonel_morales20@hotmail.com>
2022-03-28 12:24:42 -0500

    animal route

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,9 +3,11 @@ const router = require("express").Router();
 const userRoutes = require("./user-routes");
 const postRoutes = require("./post-routes");
 const commentRoutes = require("./comment-routes");
+const animalRoutes = require("./animal-routes");
 
 router.use("/users", userRoutes);
 router.use("/posts", postRoutes);
 router.use("/comments", commentRoutes);
+router.use("/animals", animalRoutes);
 
 module.exports = router;
