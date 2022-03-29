df3715e8d7f7313041f84da30b31df1093ebeeee
Luke G <lukejgranered@gmail.com>
2022-03-29 08:12:02 -0500

    handlebars fixed

--- server.js
+++ server.js
@@ -5,17 +5,7 @@ const path = require("path");
 const sequelize = require("./config/connection");
 const { cloudinary } = require('./config/cloudinary');
 
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-const sess = {
-  secret: 'neo-pets secret',
-  cookie: {},
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-      db: sequelize
-  })
-};
 
 const app = express();
 // Handlebars templates
@@ -29,10 +19,8 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.use(routes);
-
 app.use(express.static(path.join(__dirname, "public")));
+app.use(routes);
 
 //sequelize.sync({ force: false }).then(() => {
 app.listen(PORT, () => console.log("Now listening"));
