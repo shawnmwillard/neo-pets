4ce7e2415f64c55344305abd9ad48f02b3bee055
Luke G <lukejgranered@gmail.com>
2022-03-29 19:58:58 -0500

    log in feature

--- models/User.js
+++ models/User.js
@@ -6,7 +6,8 @@ const sequelize = require("../config/connection");
 
 class User extends Model {
   checkPassword(loginPw) {
-    return bcrypt.compareSync(loginPw, this.password);
+    // return bcrypt.compareSync(loginPw, this.password);
+    return loginPw === this.password;
   }
 }
 
