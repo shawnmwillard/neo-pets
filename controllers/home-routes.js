c0e58b66f3fb014dcf6750302db3ba13a7e8bf61
EsteMaeLeo <leonel_morales20@hotmail.com>
2022-03-29 20:28:31 -0500

    adding view all post

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,23 +1,48 @@
 const router = require("express").Router();
+const { Post, User, Reaction, Comment } = require("../models");
+const sequelize = require("../config/connection");
 
 router.get("/", (req, res) => {
-  res.render("homepage", {
-    id: 1,
-    post_url: 'https://handlebarsjs.com/guide/',
-    title: 'Handlebars Docs',
-    comments: [{}, {}],
-    user: {
-      username: 'test_user'
-    }
-  });
+  Post.findAll({
+    order: [["created_at", "DESC"]],
+    attributes: [
+      "id",
+      "text",
+      "user_id",
+      "place",
+      "created_at",
+      [
+        sequelize.literal(
+          "(SELECT COUNT(*) FROM reaction WHERE post.id = reaction.post_id)"
+        ),
+        "reaction_count",
+      ],
+    ],
+
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username", "first_name", "last_name"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username", "first_name", "last_name"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      const posts = dbPostData.map((post) => post.get({ plain: true }));
+      res.render("homepage", { posts });
+      //res.render("homepage", dbPostData[0].get({ plain: true }));
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
-
-router.get('/login', (req, res) => {
-  res.render('login');
-})
-
-router.get('/signup', (req, res) => {
-  res.render('signup');
-})
 
 module.exports = router;
